b88c0873a9801209de5e95ca182f310b46d42442
sopin97 <38188297+sopin97@users.noreply.github.com>
2018-04-24 14:11:11 -0300

    Update debouncer.sv

--- debouncer/debouncer.sv
+++ debouncer/debouncer.sv
@@ -1,5 +1,5 @@
 module debouncer(
-	input logic clk,			// entrada de un reloj de frecuencia:
+	input logic slow_clk,		// entrada de un reloj de frecuencia: 400Hz
 	input logic trigger,		// entrada boton
 	input logic reset,
 	output logic clean_trigger	// salida sin ruido
@@ -7,11 +7,8 @@ module debouncer(
     logic Q1;
     logic Q2;
     logic n_Q2;
-    logic slow_clk;
-    variable_clock #400 SLOW (.clk_in(clk),.reset(reset),.clk_out(slow_clk));
     D_FF D1 (.clk(slow_clk),.D(trigger),.Q(Q1));
     D_FF D2 (.clk(slow_clk), .D(Q1),.Q(Q2));
     assign n_Q2 = ~Q2;
     assign clean_trigger = Q1 & n_Q2;     
- 
 endmodule
