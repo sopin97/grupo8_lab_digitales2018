48b01430123b6e405474162af1652fc944a237c2
sopin97 <38188297+sopin97@users.noreply.github.com>
2018-04-28 22:02:00 -0300

    Update mod_display_32bit.sv

--- Componentes_genericos/mod_display_32bit.sv
+++ Componentes_genericos/mod_display_32bit.sv
@@ -2,17 +2,32 @@
 
 module display_hex(
     input [31:0] numero_entrada,
-    input power_on, // prende el display
-    input clk_display,
+    input power_on, clk, // prende el display
 	
-    output logic [7:0] SEG,
+    output logic [6:0] SEG,
     output logic [7:0] ANODO
 	);
-    logic [2:0] contador = 3'b0;
-    logic [3:0] bus_out;  
+	logic clk_display;
+	localparam COUNTER_MAX = (100000000/(2*480))-1;
+    logic [2:0] contador_anodo = 3'b0;
+    logic [31:0] contador_display = 'd0;
+    logic [3:0] bus_out;
+    
+    always @(posedge clk) begin
+        	if (contador_display == COUNTER_MAX)
+        	begin
+        		contador_display <= 'd0;
+        		clk_display <= ~clk_display;
+        	end
+        	else
+        	begin
+        		contador_display <= contador_display + 'd1;
+        		clk_display <= clk_display;
+        	end
+        end
     
     always @(posedge clk_display) begin
-    	contador = contador + 3'b1;
+    	contador_anodo = contador_anodo + 3'b1;
     end
     
 	always_comb begin
@@ -22,53 +37,53 @@ module display_hex(
     	end
     	else 
     	begin
-     		case (contador) 
+     		case (contador_anodo) 
         		3'd0 : ANODO[7:0] = 8'b11111110;
         		3'd1 : ANODO[7:0] = 8'b11111101;
-			3'd2 : ANODO[7:0] = 8'b11111011;
-			3'd3 : ANODO[7:0] = 8'b11110111;                              
-			3'd4 : ANODO[7:0] = 8'b11101111;
-			3'd5 : ANODO[7:0] = 8'b11011111;
-			3'd6 : ANODO[7:0] = 8'b10111111;
-			3'd7 : ANODO[7:0] = 8'b01111111;        
+				3'd2 : ANODO[7:0] = 8'b11111011;
+				3'd3 : ANODO[7:0] = 8'b11110111;                              
+				3'd4 : ANODO[7:0] = 8'b11101111;
+				3'd5 : ANODO[7:0] = 8'b11011111;
+				3'd6 : ANODO[7:0] = 8'b10111111;
+				3'd7 : ANODO[7:0] = 8'b01111111;        
           		default : ANODO[7:0] = 8'b11111111;
       		endcase
  		end
  	end
 
 	always_comb begin
-    	case (contador) 
-    			3'd0 :  bus_out = numero_entrada[3:0];
+    	case (contador_anodo) 
+    		3'd0 :  bus_out = numero_entrada[3:0];
 			3'd1 :  bus_out = numero_entrada[7:4];
 			3'd2 :  bus_out = numero_entrada[11:8];
 			3'd3 :  bus_out = numero_entrada[15:12];
 			3'd4 :  bus_out = numero_entrada[19:16];
 			3'd5 :  bus_out = numero_entrada[23:20];
 			3'd6 :  bus_out = numero_entrada[27:24];
-			3'd7 :  bus_out = numero_entrada[31:28]; 
-			default: bus_out = 4'b0000;
+			3'd7 :  bus_out = numero_entrada[31:28];
+			default: bus_out = 4'b0000;                            
     	endcase
  	end
     
 	always_comb begin
     	case(bus_out)
-			4'd0:  SEG = 8'b00000011;
-			4'd1:  SEG = 8'b10011111; 
-			4'd2:  SEG = 8'b00100101;
-			4'd3:  SEG = 8'b00001101;
-			4'd4:  SEG = 8'b10011001;
-			4'd5:  SEG = 8'b01001001;
-			4'd6:  SEG = 8'b01000001;
-			4'd7:  SEG = 8'b00011111;
-			4'd8:  SEG = 8'b00000001;
-			4'd9:  SEG = 8'b00011001;
-			4'd10: SEG = 8'b00010001; 
-			4'd11: SEG = 8'b11000001;
-			4'd12: SEG = 8'b01100011;
-			4'd13: SEG = 8'b10000101;
-			4'd14: SEG = 8'b01100001;
-			4'd15: SEG = 8'b01110001;
-			default: SEG = 8'b11111111;
+			4'd0:  SEG = 7'b0000001;
+			4'd1:  SEG = 7'b1001111; 
+			4'd2:  SEG = 7'b0010010;
+			4'd3:  SEG = 7'b0000110;
+			4'd4:  SEG = 7'b1001100;
+			4'd5:  SEG = 7'b0100100;
+			4'd6:  SEG = 7'b0100000;
+			4'd7:  SEG = 7'b0001111;
+			4'd8:  SEG = 7'b0000000;
+			4'd9:  SEG = 7'b0001100;
+			4'd10: SEG = 7'b0001000; 
+			4'd11: SEG = 7'b1100000;
+			4'd12: SEG = 7'b0110001;
+			4'd13: SEG = 7'b1000010;
+			4'd14: SEG = 7'b0110000;
+			4'd15: SEG = 7'b0111000;
+			default: SEG = 7'b1111111;
      	endcase
  	end
 endmodule
