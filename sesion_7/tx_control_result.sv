33e973b409e139197ab6e2db3e9a9128814e9d8b
sopin97 <38188297+sopin97@users.noreply.github.com>
2018-07-31 03:26:56 -0400

    Update tx_control_result.sv

--- sesion_7/tx_control_result.sv
+++ sesion_7/tx_control_result.sv
@@ -3,23 +3,22 @@ module tx_control(
     input logic trigger,
     input logic [15:0] raw_data,
     output logic tx_start,
-    output logic [7:0] tx_data
+    output logic [7:0] tx_data,
     //temporal
-    //output logic [2:0] id
+    output logic [2:0] id
     );
     
     //Declaracion estados modulo
     enum logic [2:0] {IDLE,Wait_register,Send_0, Wait1, Send_1, Wait2} state, next_state;
-    //assign  id  =   state;
+    assign  id  =   state;
     
     //Logica interna
     logic [15:0] temp_data, next_data; //Registro del resultado
-    logic [7:0] tx_next;
-
+    logic next_start;
     always_comb begin
         //defaults
         next_state  =   state;
-        tx_start    =   'd0;
+        next_start = 1'b0;
         next_data   =   temp_data; //revisar
         tx_next     =   tx_data;
         
@@ -34,9 +33,11 @@ module tx_control(
                 next_state  =   Send_0;
                 end        
             Send_0: begin
-                tx_start    =   1'b1;
+                next_start    =   1'b1;
                 tx_next     =   temp_data[7:0];
-                next_state = Wait1;
+                if (tx_start == 1'b1) begin
+                     next_state = Wait1;   
+                end
                 end
             Wait1: begin
                 if (~tx_busy) begin
@@ -45,16 +46,18 @@ module tx_control(
             end
             Send_1: begin
                 
-                tx_start    =   1'b1;
-                tx_next     =   temp_data[15:8];
-                next_state = Wait2;
+                next_start    =   1'b1;
+                tx_next     =   temp[15:8];
+                if (tx_start == 1'b1) begin
+                     next_state = Wait2;   
                 end
+                end
+            endcase
             Wait2: begin
                 if (~tx_busy) begin
                    next_state = IDLE; 
                 end
             end
-            endcase
     end
     
     always_ff @(posedge clk) begin
@@ -64,7 +67,8 @@ module tx_control(
         else begin
         temp_data   <=  next_data;  
         state           <=  next_state;  
-        tx_data         <=  tx_next;   
+        tx_data         <=  tx_next;
+        tx_start <= next_start;
         end
     end
             
