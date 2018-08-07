0eab8eaf201d9ebd152d0b200ae015837be9bd9d
sopin97 <38188297+sopin97@users.noreply.github.com>
2018-08-06 23:26:17 -0400

    Update and rename top_con_template_completo to top_con_template_completo.sv

--- sesion_8/top_con_template_completo.sv
+++ sesion_8/top_con_template_completo.sv
@@ -75,7 +75,27 @@ module hex_to_ascii(
 	input [3:0] hex_num,
 	output logic[7:0] ascii_conv
 	);
-
+	always_comb begin
+		case (hex_num)
+			4'h0: ascii_conv = "0";
+			4'h1: ascii_conv = "1";
+			4'h2: ascii_conv = "2";
+			4'h3: ascii_conv = "3";
+			4'h4: ascii_conv = "4";
+			4'h5: ascii_conv = "5";
+			4'h6: ascii_conv = "6";
+			4'h7: ascii_conv = "7";
+			4'h8: ascii_conv = "8";
+			4'h9: ascii_conv = "9";
+			4'hA: ascii_conv = "A";
+			4'hB: ascii_conv = "B";
+			4'hC: ascii_conv = "C";
+			4'hD: ascii_conv = "D";
+			4'hE: ascii_conv = "E";
+			4'hF: ascii_conv = "F";
+			default: ascii_conv = " ";
+		endcase
+	end
 	//fill here
 endmodule
 
@@ -92,9 +112,30 @@ endmodule
  */
 module hex_to_bit_ascii(
 	input [3:0]num,
-	output [4*8-1:0]bit_ascii
+	output logic [4*8-1:0]bit_ascii
 	);
-
+	
+	always_comb begin
+		case (num)
+			4'h0: bit_ascii = "0000";
+			4'h1: bit_ascii = "0001";
+			4'h2: bit_ascii = "0010";
+			4'h3: bit_ascii = "0011";
+			4'h4: bit_ascii = "0100";
+			4'h5: bit_ascii = "0101";
+			4'h6: bit_ascii = "0110";
+			4'h7: bit_ascii = "0111";
+			4'h8: bit_ascii = "1000";
+			4'h9: bit_ascii = "1001";
+			4'hA: bit_ascii = "1010";
+			4'hB: bit_ascii = "1011";
+			4'hC: bit_ascii = "1100";
+			4'hD: bit_ascii = "1101";
+			4'hE: bit_ascii = "1110";
+			4'hF: bit_ascii = "1111";
+			default: bit_ascii = " ";
+		endcase
+		end
 	//fill Here
 	
 endmodule
