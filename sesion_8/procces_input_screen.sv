1775d1b8d6f2dcdbac10b33ddcb58ef01473eb88
sopin97 <38188297+sopin97@users.noreply.github.com>
2018-08-07 03:20:20 -0400

    Create procces_input_screen.sv

---
+++ sesion_8/procces_input_screen.sv
@@ -0,0 +1,32 @@
+`timescale 1ns / 1ps
+
+module procces_input_screen(
+	input logic [4:0] val,
+	input logic enter_button,
+	input logic clk,
+	input logic rst,
+	output logic [15:0] output_number
+    );
+    logic [15:0] next_output;
+    logic [19:0] temp;
+    always_comb begin
+    	temp = {output_number, val[3:0]};
+    	case(enter_button)
+    		1'b0:;
+    		1'b1: begin
+    			if ((val[4] == 0) && (temp[19:16] != 'd0)) begin
+    				temp = temp << 4;
+    			end
+    		end
+    	endcase
+    end
+    
+    always_ff @(posedge clk) begin
+    	if (rst) begin
+    		output_number <= 'd0;
+    	end
+    	else begin
+    		output_number <= temp[19:4];
+    	end
+    end
+endmodule
