30710badcdeef19a61ebcb59fd723f0cc1695082
sopin97 <38188297+sopin97@users.noreply.github.com>
2018-08-18 22:51:38 -0300

    Update ram_reader.sv

--- sesion_9/vga_serial_display/ram_reader.sv
+++ sesion_9/vga_serial_display/ram_reader.sv
@@ -1,70 +1,44 @@
-// RAM_WIDTH -> cantidad de bits que guarda informacion
+// RAM_WIDTH -> tama;o de una palabra
 // RAM_HEIGHT -> numero de "slots" en la RAM
 module RAM_reader #(parameter RAM_WIDTH = 8, parameter RAM_DEPTH = (1024*768*3*8)/RAM_WIDTH)
 (
-  input logic [RAM_DEPTH-1:0] data,
-  input logic rst, clk,
+  input logic [RAM_WIDTH-1:0] data,
+  input logic rst, clk, refresh_data,
   output logic [ADRESS_BITS-1:0] adress,
-  output logic [7:0] tx_data,
-  output logic tx_start, read_enable
+  output logic [RAM_WIDTH-1:0] data_out
 );
+  
   localparam ADRESS_BITS = $clog2(RAM_DEPTH); // numero de bits de adress
-  
-  enum logic [2:0] {START, READ_DATA, WAIT, SEND_DATA, REFRESH_ADRESS} next_state, state;
   logic [ADRESS_BITS-1:0] next_adress;
-  logic [7:0] next_output;
   
-  localparam MAX_COUNTER = 100000;
-  
-  logic [31:0] counter, next_counter;
-  
+  enum logic [1:0] {IDLE, READ_DATA, REFRESH_ADRESS, WAIT} next_state, state;
+  logic [ADRESS_BITS-1:0] next_adress;
+  logic [RAM_WIDTH-1:0] next_output;
+
   always_ff @(posedge clk) begin
     if (rst) begin
-      counter <= 'd0;
-      tx_data = 'd0;
-      state <= START;
-      adress <= 'd0;
-    end
-    else begin
-      counter <= next_counter;
-      tx_data = next_output;
-      state <= next_state;
-      if (adress >= RAM_DEPTH)
-        adress <= 'd0;
-      else
-        adress <= next_adress;
-  end
+      next_output <= 'd0;
+      next_adress <= 'd0;
   
   always_comb begin
     next_state = state;
+    next_output = data_out;
     next_adress = adress;
-    next_output = tx_data;
-    next_counter = 'd0;
-    tx_start = 1'b0;
-    read_enable = 1'b0
     case(state)
-      START:  begin
-        next_state = SET_ADRESS;
-        next_adress = 'd0;
-        next_output = 'd0;
+      IDLE: begin
+        if (refresh_data)
+          next_state = READ_DATA;
       end
       READ_DATA: begin
-        read_enable = 1'b1;
-        next_state = SEND_DATA;
         next_output = data;
-      SEND_DATA: begin
-        next_state = WAIT;
-        tx_start = 1'b1;
+        next_state = REFRESH_ADRESS;
       end
-      WAIT: begin
-        next_counter = counter + 'd1;
-        if (counter >= COUNTER_MAX)
-          next_state = REFRESH_ADRESS;
-      end
-      REFRESH_ADRESS: begin
-        next_state = READ_DATA;
+      REFRESH_ADRESS:
         next_adress = adress + 'd1;
-    endcase
+        next_state = WAIT;
+      WAIT:
+        if (!refresh_data)
+          next_state = IDLE;
+      
   end
-
-endmodule
+  endmodule
