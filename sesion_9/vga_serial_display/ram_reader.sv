80fc5651ae8d702276894dd6570fa898e5533bd1
sopin97 <38188297+sopin97@users.noreply.github.com>
2018-08-19 12:48:55 -0300

    Update ram_reader.sv

--- sesion_9/vga_serial_display/ram_reader.sv
+++ sesion_9/vga_serial_display/ram_reader.sv
@@ -3,46 +3,29 @@
 module RAM_reader #(parameter RAM_WIDTH = 32, parameter RAM_DEPTH = (480*360*24)/RAM_WIDTH)
 (
   input logic [RAM_WIDTH-1:0] data,
-  input logic rst, clk, pixel_clk,
+  input logic rst, clk, refresh_data,
   input logic visible,
   output logic [ADRESS_BITS-1:0] adress,
   output logic [RAM_WIDTH-1:0] data_out
 );
-  localparam MAX_ADRESS = 129600 - 1;
+  localparam MAX_ADRESS = 172800 - 1;
   localparam ADRESS_BITS = $clog2(RAM_DEPTH); // numero de bits de adress
   logic [ADRESS_BITS-1:0] next_adress;
   
   enum logic [1:0] {IDLE, READ_DATA, REFRESH_ADRESS, WAIT_READY} next_state, state;
 
   logic [RAM_WIDTH-1:0] next_output;
-  logic refresh_data;
-  
-  always_ff@(posedge clk) begin
-    if(visible) begin
-      refresh_data <= pixel_clk;
-    end
-    else begin
-      refresh_data <= 'd0;
-    end
-  end
 
   always_ff @(posedge clk) begin
     if (rst) begin
-      data_out <= 'd0;
-      adress <= 'd0;
-      state <= IDLE;
+      	data_out <= 'd0;
+     	adress <= 'd0;
+      	state <= IDLE;
     end
     else begin
-      if(visible) begin
-        data_out <= next_output;
-        adress <= next_adress;
-        state <= next_state;
-      end
-      else begin
-        data_out <= 'd0;
-        adress <=next_adress;
-        state <= next_state;
-      end
+    	data_out <= next_output;
+    	adress <= next_adress;
+     	state <= next_state;
     end
    end
   always_comb begin
@@ -55,15 +38,20 @@ module RAM_reader #(parameter RAM_WIDTH = 32, parameter RAM_DEPTH = (480*360*24)
           next_state = READ_DATA;
       end
       READ_DATA: begin
-        next_output = data;
+      	if (visible)
+        	next_output = data;
+        else
+        	next_output = 'd0;
         next_state = REFRESH_ADRESS;
       end
       REFRESH_ADRESS: begin
         next_state = WAIT_READY;
         if (adress >= MAX_ADRESS)
-          next_adress = adress + 'd0;
-        else
-          next_adress = adress + 'd1;
+          next_adress = 'd0;
+        else begin
+		  if (visible)
+          	next_adress = adress + 'd1;
+         end
       end
       WAIT_READY: begin
         if (!refresh_data)
