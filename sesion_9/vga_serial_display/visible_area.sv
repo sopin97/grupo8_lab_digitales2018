39902d99f5927303a0efbcd9d7be3700f56273b5
sopin97 <38188297+sopin97@users.noreply.github.com>
2018-08-20 05:12:37 -0300

    Update visible_area.sv

--- sesion_9/vga_serial_display/visible_area.sv
+++ sesion_9/vga_serial_display/visible_area.sv
@@ -10,17 +10,10 @@ module visible_area #(parameter X_POS = 0, parameter Y_POS = 0) (
   logic [10:0]hc_visible;
   logic [10:0]vc_visible;
   logic next_visible;
-  
-  always_ff @(posedge clk) begin
-    if (rst)
-      visible <= 'd0;
-    else
-      visible <= next_visible;
-  end
 	
 	assign hc_visible = ((hc <= (X_POS + WIDTH)) && (hc > (X_POS)))?(hc - X_POS)):11'd0;
 	assign vc_visible = ((vc <= (Y_POS + HEIGHT)) && (vc > (Y_POS)))?(vc - Y_POS):11'd0;
   
-	assign  next_visible = (|hc_visible) & (|vc_visible);
+	assign  visible = (|hc_visible) & (|vc_visible);
 
 endmodule
