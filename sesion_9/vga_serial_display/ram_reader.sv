8c7843b15b3b50c8ba999b8398e6d87ac7923bbe
Wauro21 <38472214+Wauro21@users.noreply.github.com>
2018-08-20 02:33:32 -0300

    Update ram_reader.sv

--- sesion_9/vga_serial_display/ram_reader.sv
+++ sesion_9/vga_serial_display/ram_reader.sv
@@ -8,7 +8,7 @@ module RAM_reader #(parameter RAM_WIDTH = 32)
   output logic [RAM_WIDTH-1:0] data_out
 );
   localparam RAM_DEPTH = (480*360*24)/RAM_WIDTH
-  localparam MAX_ADRESS = 172800 - 1;
+  localparam MAX_ADRESS = RAM_DEPTH - 1;
   localparam ADRESS_BITS = $clog2(RAM_DEPTH); // numero de bits de adress
 	
   logic [ADRESS_BITS-1:0] next_adress;
