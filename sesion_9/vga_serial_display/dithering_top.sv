7425f4c0b6b37eed714d2503852dda725bc74c65
sopin97 <38188297+sopin97@users.noreply.github.com>
2018-08-21 16:13:27 -0300

    Update dithering_top.sv

--- sesion_9/vga_serial_display/dithering_top.sv
+++ sesion_9/vga_serial_display/dithering_top.sv
@@ -5,19 +5,19 @@ module dithering_top (
 );
   logic [3:0] dith_red_out, dith_green_out, dith_blue_out;
   dithering_8bit RED_dithering(
-    .entrada_color_8_bit(data_in[7:4]),
+    .entrada_color_8_bit(data_in[7:0]),
     .clk(clk), .rst(rst),
     .salida_color_8_bit(dith_red_out),
     .visible(visible)
 );
   dithering_8bit GREEN_dithering(
-    .entrada_color_8_bit(data_in[15:12]),
+    .entrada_color_8_bit(data_in[15:8]),
     .clk(clk), .rst(rst),
     .salida_color_8_bit(dith_green_out),
     .visible(visible)
 );
   dithering_8bit BLUE_dithering(
-    .entrada_color_8_bit(data_in[23:20]),
+    .entrada_color_8_bit(data_in[23:16]),
     .clk(clk), .rst(rst),
     .salida_color_8_bit(dith_blue_out),
     .visible(visible)
@@ -26,7 +26,7 @@ module dithering_top (
   always_comb begin
     case (SW)
       1'b0: data_out = data_in;
-      1'b1: data_out = (visible == 1'b1)? {dith_blue_out, 4'b0 , dith_green_out, 4'b0, dith_red_out, 4'b0}:24'd0;
+      1'b1: data_out = (visible == 1'b1)? {dith_blue_out,  dith_green_out, dith_red_out, 4'b0}:24'd0;
     endcase
   end
 endmodule
