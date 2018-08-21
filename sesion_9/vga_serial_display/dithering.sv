56cd714833d5297dba22ced92473fa28b9a1a09e
sopin97 <38188297+sopin97@users.noreply.github.com>
2018-08-21 16:55:15 -0300

    Update dithering.sv

--- sesion_9/vga_serial_display/dithering.sv
+++ sesion_9/vga_serial_display/dithering.sv
@@ -8,7 +8,7 @@ localparam THRESHOLD = 'd4;
 // cables para calcular error
 	logic [7:0] error, next_error;
 // resultado y overflow ALU
-	logic [8:0] resultado;
+	logic [15:0] resultado;
 logic overflow;
 
 // se separara a la salida de la ALU como MSN (most significant Nibble) y LSN (Less significant Nibble)
@@ -16,18 +16,16 @@ logic [3:0] MSN, LSN;
 	assign MSN = resultado[7:4];
 	assign LSN = resultado [3:0];
 
-	ALU_generalizado #(.n_bits(9)) ALU(
-	.entrada_a({1'b0,entrada_color_8_bit}), .entrada_b({1'b0,error}), // Entradas ALU
+	ALU_generalizado #(.n_bits(16)) ALU(
+		.entrada_a({8'b0,entrada_color_8_bit}), .entrada_b({8'd0,error}), // Entradas ALU
 	.operacion(3'b000), // SUMA
 	.resultado(resultado), // Resultado de la operacion
-	.overflow(overflow)
     );
     
 always_comb begin
-	if (!overflow) begin
 		if (LSN >= THRESHOLD) begin
 			next_error = error - (8'd16 - {4'd0 , LSN});
-			if (MSN == 4'hF) begin
+			if (resultado >= 8'hFF) begin
 				salida_color_8_bit = {MSN , 4'd0};
 			end
 			else begin
@@ -38,10 +36,7 @@ always_comb begin
 			next_error = error + {4'd0 , LSN};
 			salida_color_8_bit = {MSN , 4'd0};
     		end
-  	end
-	else begin
-		next_error = error - {MSN , LSN}+ 'd1;
-		salida_color_8_bit = {4'hF , 4'd0};
+salida_color_8_bit = {4'hF , 4'd0};
   	end
 end
 	// logica secuencial del error
