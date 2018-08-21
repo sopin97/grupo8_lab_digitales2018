baa077072927b3da66fdaf671892d36153fe5add
Wauro21 <38472214+Wauro21@users.noreply.github.com>
2018-08-21 12:51:32 -0300

    Update grayscaler.sv

--- sesion_9/vga_serial_display/grayscaler.sv
+++ sesion_9/vga_serial_display/grayscaler.sv
@@ -1,1 +1,14 @@
-
+module gray_scaler (
+	input logic SW, //SWITCH 1
+	input logic [23:0] input_line,
+	output logic [23:0] gray_scaled
+);
+	always_comb begin
+		//defaults
+		case(SW)
+			1'b0:	gray_scaled = input_line;
+			1'b1:	gray_scaled = (((input_line[7:0] *30)/10) + ((input_line[15:8] *59)/100) + ((input_line[23:16] *11)/100));
+			default: gray_scaled = input_line;
+		endcase
+	end
+endmodule
