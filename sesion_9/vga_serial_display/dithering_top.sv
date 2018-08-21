04bae10db5c26a10a59d2f763ed4cb067d326f28
sopin97 <38188297+sopin97@users.noreply.github.com>
2018-08-21 13:40:25 -0300

    Update dithering_top.sv

--- sesion_9/vga_serial_display/dithering_top.sv
+++ sesion_9/vga_serial_display/dithering_top.sv
@@ -1,9 +1,29 @@
 module dithering_top (
   input logic [23:0] data_in,
   output logic [23:0] data_out,
-  input logic clk, rst
+  input logic clk, rst, SW
+);
+  logic [7:0] dith_red_out, dith_green_out, dith_blue_out;
+  dithering_8bit RED_dithering(
+    .entrada_color_8_bit(data_in[7:0]),
+    .clk(clk), .rst(rst),
+    .salida_color_8_bit(dith_red_out)
+);
+  dithering_8bit GREEN_dithering(
+    .entrada_color_8_bit(data_in[15:8]),
+    .clk(clk), .rst(rst),
+    .salida_color_8_bit(dith_green_out)
+);
+  dithering_8bit BLUE_dithering(
+    .entrada_color_8_bit(data_in[23:16]),
+    .clk(clk), .rst(rst),
+    .salida_color_8_bit(dith_blue_out)
 );
 
-always_comb begin
-
-end
+  always_comb begin
+    case (SW)
+      1'b0: data_out = data_in;
+      1'b1: data_out = {dith_blue_out , dith_green_out, dith_red_out};
+    endcase
+  end
+endmodule
